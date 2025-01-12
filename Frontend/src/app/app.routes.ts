ebc31184a21a7d6a56b0aeee707ebe89473b4b89
Zablo100 <ZabloEmail@gmail.com>
2025-01-12 19:38:04 +0100

    Wyświetlanie strony tylko dla zalogowanych użytkowników

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { PrjektPageComponent } from '../projekt-page/prjekt-page.component';
 import { NgModule } from '@angular/core';
 import {LoginPageComponent} from "../login-page/login-page.component";
 import {RegisterComponent} from "../register-page/register.component";
+import { AuthGuard } from './guard';
 
 export const routes: Routes = [
-    {path: "projekty", component: ProjektListPageComponent },
-    {path: "projekt/:id", component: PrjektPageComponent},
+    {path: "projekty", component: ProjektListPageComponent, canActivate: [AuthGuard] },
+    {path: "projekt/:id", component: PrjektPageComponent, canActivate: [AuthGuard]},
     {path: "login", component: LoginPageComponent},
   {path: "register", component: RegisterComponent}
 
